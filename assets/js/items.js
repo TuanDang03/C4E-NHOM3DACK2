26d36d9479132bcd45caa3258b1e0cb61967f584
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-02-16 18:36:41 +0700

    Update items.js

--- assets/js/items.js
+++ assets/js/items.js
@@ -1,25 +1,5 @@
-function myFunctionInput() {
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("items");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-      } else {
-          li[i].style.display = "none";
-      }
-  }
-}
-
-
-
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-
 const usersElement = document.querySelector('.items');
 usersElement.innerHTML = "";
 let htmlData = "";
