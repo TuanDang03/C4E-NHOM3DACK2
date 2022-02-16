c1e287db45036b3594266325142fb6e655020d65
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-02-16 18:36:24 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,3 @@
-function myFunctionInput() {
-    var input, filter, ul, li, div, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("champions");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("div")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-  
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 const usersElement = document.querySelector('.champions');
