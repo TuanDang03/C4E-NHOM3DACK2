5e6ce1a3f43ffac90786df954c64430249e5679f
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-02-16 21:12:40 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,29 @@
-const urlSearchParams = new URLSearchParams(window.location.search);
-const params = Object.fromEntries(urlSearchParams.entries());
-const usersElement = document.querySelector('.champions');
+const urlSearchParams = new URLSearchParams(window.location.search)
+const params = Object.fromEntries(urlSearchParams.entries())
+const usersElement = document.querySelector('.champions')
 usersElement.innerHTML = "";
 let htmlData = "";
 if(params.lane){
-    for(const item of champions){
+    for(item of champions){
         if(item.lane.includes(params.lane)){
             let html = `
-    <li >
-        <img class="image" src="${item.image}" height="120" alt="">
-        <a class="click" href="${item.link}"><div class="name">${item.name}</div></a>
-    </li>`;
-    htmlData +=html;
+            <li >
+            <img class="image" src="${item.image}" height="120" alt="">
+            <a class="click" href="${item.link}"><div class="name">${item.name}</div></a>
+            </li>`
+            htmlData += html
         }
     }
-}else{
-    for(const item of champions){
-        let html = `
-    <li >
-        <img class="image" src="${item.image}" height="120" alt="">
-        <a class="click" href="${item.link}"><div class="name">${item.name}</div></a>
-    </li>`;
-    htmlData +=html;
 }
+else{
+    for(item of champions){
+            let html = `
+            <li >
+            <img class="image" src="${item.image}" height="120" alt="">
+            <a class="click" href="${item.link}"><div class="name">${item.name}</div></a>
+            </li>`
+            htmlData += html
+    }
 }
 usersElement.innerHTML = htmlData;
 
