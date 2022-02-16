daff9aa820d828914eaeaf6833f16a73a5af84a0
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-02-16 18:37:08 +0700

    Update runes.js

--- assets/js/runes.js
+++ assets/js/runes.js
@@ -1,25 +1,3 @@
-function myFunctionInput() {
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("champions");
-  li = ul.getElementsByTagName("li");
-  
-  const inputDisplay = input.value.length > 1 ? 'block' : 'none';
-  ul.style.display = inputDisplay;
-  
-  for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-      } else {
-          li[i].style.display = "none";
-      }
-  }
-  }
-
-
 const urlSearchParams = new URLSearchParams(window.location.search)
 const params = Object.fromEntries(urlSearchParams.entries())
 const usersElement = document.querySelector('.runes')
@@ -46,11 +24,9 @@ if(params.type){
       htmlData += html;
       i++;
     }
-    
   }
 }else{
 }
-
 usersElement.innerHTML = htmlData;
 
 
